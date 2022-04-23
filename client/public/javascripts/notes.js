857d39ac0cfadd21c6e16664a47aa99412706f16
MrtnBck <martin.dev.dev@gmail.com>
2022-04-23 16:17:51 +0200

    Add notes logic part II.

--- client/public/javascripts/notes.js
+++ client/public/javascripts/notes.js
@@ -3,47 +3,62 @@ const openBtn = document.getElementById('notes-open');
 let textArea, closeBtn, saveBtn, changeNoteEl, changeBtns;
 //addBtn
 
-const texts = ['0', '1', '2', '3', '4'];
+const texts = ['Note-0', 'Note-1', 'Note-2', 'Note-3', 'Note-4'];
 
 openBtn.addEventListener('click', () => {
-  noteEl.innerHTML = `
-    <div class="notes-container">
-    <div class="notes-outer">
-    
-    </div>
-    <div class="notes-main-container">
-        <div class="control-note-btns">
-            <button class="btn" id="save">S</button>
-            <button class="btn" id="remove"><i class="fa-solid fa-trash-can"></i></button>
-            <button class="btn" id="notes-close"><i class="fa-solid fa-xmark"></i></button>
-        </div>
-        <textarea id="note-area"></textarea>
-        </div>
-    </div>`;
-  textArea = document.getElementById('note-area');
-  closeBtn = document.getElementById('notes-close');
-  removeBtn = document.getElementById('remove');
-  //addBtn = document.getElementById('add');
-  saveBtn = document.getElementById('save');
-  changeNoteEl = document.querySelector('.notes-outer');
-  drawChangeBtns(5);
-  textArea.value = getText(0);
+  openBtn.classList.add('slide-off-btn');
+  setTimeout(function () {
+    noteEl.innerHTML = `
+      <div class="notes-container">
+      <div class="notes-outer">
+      
+      </div>
+      <div class="notes-main-container">
+          <div class="control-note-btns">
+              <button class="btn" id="save"><i class="fa-solid fa-floppy-disk"></i></button>
+              <button class="btn" id="remove"><i class="fa-solid fa-trash-can"></i></button>
+              <button class="btn" id="notes-close"><i class="fa-solid fa-xmark"></i></button>
+          </div>
+          <textarea id="note-area"></textarea>
+          </div>
+      </div>`;
 
-  saveBtn.addEventListener('click', () => {
-    console.log(getActiveBtn());
-  });
+    textArea = document.getElementById('note-area');
+    closeBtn = document.getElementById('notes-close');
+    removeBtn = document.getElementById('remove');
+    saveBtn = document.getElementById('save');
+    changeNoteEl = document.querySelector('.notes-outer');
+    drawChangeBtns(5);
+    textArea.value = getText(0);
+
+    saveBtn.addEventListener('click', () => {
+      const idx = getActiveBtn();
+      setText(idx, textArea.value);
+    });
+
+    removeBtn.addEventListener('click', () => {
+      const idx = getActiveBtn();
+      setText(idx, '');
+      textArea.value = '';
+    });
+
+    closeBtn.addEventListener('click', () => {
+      const notesContainer = document.querySelector('.notes-container');
+      notesContainer.classList.add('slide-off');
+      setTimeout(function () {
+        notesContainer.remove();
+        openBtn.classList.remove('slide-off-btn');
+      }, 500);
+    });
+  }, 500);
 });
 
 const getActiveBtn = () => {
-  changeBtns.forEach((btn, idx) => {
-    //console.log(btn);
+  for (const [idx, btn] of changeBtns.entries()) {
     if (btn.classList.contains('active')) {
-      console.log(idx);
       return idx;
-    } else {
-      return false;
     }
-  });
+  }
 };
 
 const setText = (idx, text) => {
@@ -58,10 +73,12 @@ const setText = (idx, text) => {
 
 const getText = (idx) => {
   const texts = JSON.parse(localStorage.getItem('notes'));
-  if (idx === undefined) {
-    return texts;
-  } else {
+  if (localStorage.getItem('notes') !== null) {
     return texts[idx];
+  } else {
+    localStorage.setItem('notes', JSON.stringify(texts));
+    console.log('asdasd');
+    return texts[0];
   }
 };
 
@@ -69,11 +86,13 @@ const drawChangeBtns = (qty) => {
   for (let i = 0; i < qty; i++) {
     const changeBtn = document.createElement('button');
     changeBtn.classList.add('btn', 'btn-change');
+    if (i === 0) {
+      changeBtn.classList.add('active');
+    }
     changeBtn.id = `change-${i}`;
     changeBtn.innerHTML = '<i class="fa-solid fa-note-sticky"></i>';
     changeBtn.addEventListener('click', () => {
       textArea.value = changeNote(changeBtn, i);
-      //console.log('clicked');
     });
     changeNoteEl.appendChild(changeBtn);
   }
